ba8369e02891cd8c9197051c5da14adbb48caaa3
marikoZz <mariuslange11@gmail.com>
2025-11-09 22:03:54 +0100

    NixOS: Config Update 2025-11-09 22:03:54

--- home.nix
+++ home.nix
@@ -82,5 +82,15 @@
     # Firefox settings and profiles can be configured here [cite: 6]
   };
 
+  # Erstellt eine globale Flag-Datei für VSCode.
+  # Diese wird *immer* gelesen, egal wie VSCode gestartet wird.
+  home.file.".config/code-flags.conf" = {
+    text = ''
+      # Jedes Flag in eine eigene Zeile
+      --enable-features=UseOzonePlatform
+      --ozone-platform=wayland
+    '';
+  };
+
   # Other program configurations can be added here
 }
